abe01aa67f813d996805434a105fdacbcfd81bc3
Mahendra Dani <danimahendra0904@gmail.com>
2023-09-27 22:58:49 +0530

    feat: created about us section with raw content

---
+++ client/src/components/About/About.tsx
@@ -0,0 +1,36 @@
+const About = () => {
+  return (
+    <div className="w-full bg-[#0F1B92] text-white">
+      <div className="p-4 py-8 text-center lg:w-[70%] mx-auto space-y-4">
+        <h1 className="text-3xl font-bold">ABOUT US</h1>
+        <p className="text-lg">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum
+          fugiat voluptate magni iusto repellat autem alias quasi nisi. Veniam
+          magnam architecto natus laudantium amet, in perspiciatis sint voluptas
+          est sunt assumenda, minus eum neque officiis provident quae dolor
+          tempora quasi explicabo ratione ad placeat nesciunt. Temporibus
+          dolores labore, corrupti quidem adipisci eaque exercitationem.
+          Deleniti provident sequi delectus nulla similique sit a totam
+          asperiores excepturi dignissimos, fugiat architecto ab temporibus ut
+          laboriosam unde doloremque nobis dolores natus illo, reiciendis nihil
+          rem tenetur molestias? Sunt, labore iste. Aperiam ipsam a, quasi
+          beatae, ipsa autem atque molestiae maxime aliquam nesciunt, dolore
+          distinctio non. Voluptas, laudantium, exercitationem provident vel
+          minus temporibus aperiam, laborum quis fugiat illo eaque libero iste!
+          Eos soluta quisquam aspernatur corrupti at, neque doloremque ratione
+          tenetur dignissimos quidem adipisci veritatis provident quis nam sequi
+          asperiores omnis assumenda dolorem dolorum eaque harum! Nesciunt
+          corporis, alias commodi vel odio magnam ipsum amet dignissimos
+          adipisci aperiam iure provident esse aliquam eos tempore. Labore
+          recusandae quae, suscipit nulla eaque quasi, dolores consequatur eos
+          voluptates repellat ipsum obcaecati animi aliquam quisquam cupiditate
+          ab praesentium perspiciatis consequuntur, assumenda dolor. Consectetur
+          earum nesciunt atque, vero placeat consequuntur quos nulla blanditiis
+          exercitationem ipsum sit necessitatibus dolorem magnam aperiam aut?
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default About;
--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -4,12 +4,14 @@ import Header from "../components/Header";
 // import indiaMap from "../../public/images/india.png";
 import Weather from "../components/Weather/Weather";
 import Footer from "../components/Footer";
+import About from "../components/About/About";
 
 const Home: React.FC = () => {
   return (
     <>
       <Navbar />
       <Header />
+      <About />
       {/* <Weather /> */}
       {/* <Footer /> */}
       {/* <div className="py-12 w-full grid place-content-center">
