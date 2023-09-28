1131f1803a3b23fc936228b196a45514748409a4
Mahendra Dani <danimahendra0904@gmail.com>
2023-09-28 11:21:17 +0530

    feat: added cards in About section

--- client/src/components/About/About.tsx
+++ client/src/components/About/About.tsx
@@ -1,33 +1,46 @@
+import InfoCard from "./InfoCard";
+
+type InfoCardDetails = {
+  title: string;
+  description: string;
+};
 const About = () => {
+  const InfoCardDetails: Array<InfoCardDetails> = [
+    {
+      title: "SURAKSHATSAATHI",
+      description:
+        "Our mission is simple yet profound: to empower communities and organizations to respond effectively to disasters. With years of dedicated service in the field of disaster management, we've harnessed cutting-edge technology and a passion for making a difference.",
+    },
+    {
+      title: "WHO WE ARE?",
+      description:
+        "We are a team of experts in disaster management, technology, and community engagement. Our journey began with a vision of creating a safer world for everyone.",
+    },
+    {
+      title: "WHAT WE DO?",
+      description:
+        "We've developed an innovative platform that bridges the gap between communities and response agencies. Suraksha Saathi offers real-time information, collaboration tools, and resources, transforming disaster response into a collective effort.",
+    },
+    {
+      title: "WHY ChOOSE US?",
+      description:
+        "Our commitment to excellence, user-friendly solutions, and unwavering dedication set us apart. We're not just a platform; we're your trusted partner in disaster readiness and recovery.",
+    },
+  ];
   return (
-    <div className="w-full bg-[#0F1B92] text-white">
-      <div className="p-4 py-8 text-center lg:w-[70%] mx-auto space-y-4">
-        <h1 className="text-3xl font-bold">ABOUT US</h1>
-        <p className="text-lg">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum
-          fugiat voluptate magni iusto repellat autem alias quasi nisi. Veniam
-          magnam architecto natus laudantium amet, in perspiciatis sint voluptas
-          est sunt assumenda, minus eum neque officiis provident quae dolor
-          tempora quasi explicabo ratione ad placeat nesciunt. Temporibus
-          dolores labore, corrupti quidem adipisci eaque exercitationem.
-          Deleniti provident sequi delectus nulla similique sit a totam
-          asperiores excepturi dignissimos, fugiat architecto ab temporibus ut
-          laboriosam unde doloremque nobis dolores natus illo, reiciendis nihil
-          rem tenetur molestias? Sunt, labore iste. Aperiam ipsam a, quasi
-          beatae, ipsa autem atque molestiae maxime aliquam nesciunt, dolore
-          distinctio non. Voluptas, laudantium, exercitationem provident vel
-          minus temporibus aperiam, laborum quis fugiat illo eaque libero iste!
-          Eos soluta quisquam aspernatur corrupti at, neque doloremque ratione
-          tenetur dignissimos quidem adipisci veritatis provident quis nam sequi
-          asperiores omnis assumenda dolorem dolorum eaque harum! Nesciunt
-          corporis, alias commodi vel odio magnam ipsum amet dignissimos
-          adipisci aperiam iure provident esse aliquam eos tempore. Labore
-          recusandae quae, suscipit nulla eaque quasi, dolores consequatur eos
-          voluptates repellat ipsum obcaecati animi aliquam quisquam cupiditate
-          ab praesentium perspiciatis consequuntur, assumenda dolor. Consectetur
-          earum nesciunt atque, vero placeat consequuntur quos nulla blanditiis
-          exercitationem ipsum sit necessitatibus dolorem magnam aperiam aut?
-        </p>
+    <div className="w-full bg-[#0F1B92] text-white p-4">
+      <div className="p-4 py-8  lg:w-[80%] mx-auto space-y-4">
+        <h1 className="text-3xl font-bold text-center pb-8">ABOUT US</h1>
+        <div className="flex flex-wrap md:justify-between justify-center items-center gap-4">
+          {InfoCardDetails.map((infoCard) => {
+            return (
+              <InfoCard
+                title={infoCard.title}
+                description={infoCard.description}
+              />
+            );
+          })}
+        </div>
       </div>
     </div>
   );
---
+++ client/src/components/About/InfoCard.tsx
@@ -0,0 +1,16 @@
+interface InfoCardProps {
+  title: string;
+  description: string;
+}
+const InfoCard = ({ title, description }: InfoCardProps) => {
+  return (
+    <div className="w-60 min-h-[25rem] rounded-sm bg-[#BED6FF] space-y-3 p-4">
+      <h2 className="text-center py-4 bg-[#3848E0] text-xl font-semibold">
+        {title}
+      </h2>
+      <p className="text-[#3848E0] px-2">{description}</p>
+    </div>
+  );
+};
+
+export default InfoCard;
